7009ce36937a17ab8272e3a3750873c90b1d050e
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-01-30 13:22:13 +0100

    L5, API calls with axios

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,43 +1,21 @@
 <script setup>
-import EventCard from '@/components/EventCard.vue';
+import { ref, onMounted } from 'vue'
+import EventService from '@/services/EventService.js'
+import EventCard from '@/components/EventCard.vue'
 
-import { ref } from 'vue';
+const events = ref(null)
 
-const events = ref([
-  {
-    id: 598101,
-    category: 'animal welfare',
-    title: 'Cat Adoption Day',
-    description: 'Find you new felinf freta eatt',
-    location: 'huset',
-    date: 'January 28, 2022',
-    time: '12:00',
-    petsAllowed: true,
-    organizer: 'Kat Lyadee',
-  },
-  {
-    id: 847382,
-    category: 'food',
-    title: 'Gratis mat',
-    description: 'Spis masse mat :)',
-    location: 'Coop Mega',
-    date: 'January 30, 2022',
-    time: '08:00',
-    petsAllowed: false,
-    organizer: 'Rema',
-  },
-  {
-    id: 483756,
-    category: 'sustainability',
-    title: 'jakt på apekatt',
-    description: 'gå ut i skog og jakt apekatt, helts ikke drep den han kan bli venn',
-    location: 'Skogen',
-    date: 'March 15, 2022',
-    time: '16:40',
-    petsAllowed: true,
-    organizer: 'AntiApekattAlliansen',
-  },
-])
+onMounted(() => {
+  EventService.getEvents()
+  .then(
+    (response) => {
+      events.value = response.data
+    }
+  )
+  .catch((error) => {
+    console.log(error)
+  })
+})
 </script>
 <template>
 <h1>Events for god</h1>

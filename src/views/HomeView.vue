890a1ca4d0c833506c5013ae006b3fba6b9f8c67
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-01-30 12:31:31 +0100

    L3, single file components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,59 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+import EventCard from '@/components/EventCard.vue';
 
+import { ref } from 'vue';
+
+const events = ref([
+  {
+    id: 598101,
+    category: 'animal welfare',
+    title: 'Cat Adoption Day',
+    description: 'Find you new felinf freta eatt',
+    location: 'huset',
+    date: 'January 28, 2022',
+    time: '12:00',
+    petsAllowed: true,
+    organizer: 'Kat Lyadee',
+  },
+  {
+    id: 847382,
+    category: 'food',
+    title: 'Gratis mat',
+    description: 'Spis masse mat :)',
+    location: 'Coop Mega',
+    date: 'January 30, 2022',
+    time: '08:00',
+    petsAllowed: false,
+    organizer: 'Rema',
+  },
+  {
+    id: 483756,
+    category: 'sustainability',
+    title: 'jakt på apekatt',
+    description: 'gå ut i skog og jakt apekatt, helts ikke drep den han kan bli venn',
+    location: 'Skogen',
+    date: 'March 15, 2022',
+    time: '16:40',
+    petsAllowed: true,
+    organizer: 'AntiApekattAlliansen',
+  },
+])
+</script>
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<h1>Events for god</h1>
+  <div class="events">
+    <EventCard 
+    v-for="event in events"
+    :key="event.id"
+    :event="event"
+    />
+  </div>
 </template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>

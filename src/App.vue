e82380e557efaad9aa4ffe412727d4ce987b586c
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-01-31 11:35:34 +0100

    dynamic redirection

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink :to="{ name: 'event-list' }">Home</RouterLink> |
+          <RouterLink :to="{ name: 'event-list' }">Events</RouterLink> |
           <RouterLink :to="{name: 'about'}">About</RouterLink>
         </nav>
       </div>

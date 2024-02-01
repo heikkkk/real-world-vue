efdb96e09d5339fcaa6f091080c6c8f1d70c5ae7
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-02-01 10:03:47 +0100

    flash message for registering to an event

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,53 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { inject } from 'vue'
+
+const GStore = inject('GStore')
 </script>
 
 <template>
-  <div id="layout">
-    <header>
-      <div class="wrapper">
-        <nav>
+  <div id="app">
+    <div id="flashMessage" v-if="GStore.flashMessage">
+      {{ GStore.flashMessage }}
+    </div>
+      <div id="nav">
           <RouterLink :to="{ name: 'event-list' }">Events</RouterLink> |
           <RouterLink :to="{name: 'about'}">About</RouterLink>
-        </nav>
       </div>
-    </header>
     <RouterView />
   </div>
 </template>
 
 <style>
-#layout {
+@keyframes yellowfade {
+  from {
+    background: yellow;
+  }
+  to {
+    background: transparent;
+  }
+}
+
+#flashMessage {
+  animation-name: yellowfade;
+  animation-duration: 3s;
+}
+
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smooting: antialiased;
   -moz-osx-font-smoting: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-nav {
+#nav {
   padding: 30px;
 }
-nav a {
+#nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-nav a.router-link-exact-active {
+#nav a.router-link-exact-active {
   color: #42b983;
 }
 h2 {
--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,10 +1,16 @@
 <script setup>
-import { useRouter } from 'vue-router';
+import { useRouter } from 'vue-router'
+import {defineProps, inject } from 'vue'
 
-const props = defineProps(['event'])
+const { event } = defineProps(['event'])
 const router = useRouter()
+const GStore = inject('GStore')
 
 const register = () => {
+    GStore.flashMessage = 'You are successfully registerd for ' + event.title
+    setTimeout(() => {
+        GStore.flashMessage = ''
+    }, 3000)
     router.push({
         name: 'event-details'
     })
